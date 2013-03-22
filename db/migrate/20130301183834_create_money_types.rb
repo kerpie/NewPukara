8a60ce2b803cbb165d93fb7744ecde8837894219
Kerry Perez <k4torce@gmail.com>
2013-03-22 06:35:27 -0500

    migraciones eliminadas

--- db/migrate/20130301183834_create_money_types.rb
+++
@@ -1,9 +0,0 @@
-class CreateMoneyTypes < ActiveRecord::Migration
-	def change
-	    create_table :money_types do |t|
-			t.string :name
-      		t.float :value
-			t.timestamps
-    	end
-  	end
-end 
